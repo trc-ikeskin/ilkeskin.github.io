0648b99a8c313d17d833c351f3328774234f30c2
Ilyas Keskin <github.bzczr@simplelogin.co>
2023-03-19 21:50:44 +0100

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@ import { createApp } from "vue";
 import "./assets/css/style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+/* import specific icons */
+import {
+  faLinkedin,
+  faSquareXing,
+  faSquareTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faLinkedin, faSquareXing, faSquareTwitter);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
