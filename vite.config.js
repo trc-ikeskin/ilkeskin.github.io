f02dbf3501ef05c3841e8f92e9e2b4b21183ab20
Ilyas Keskin <github.bzczr@simplelogin.co>
2024-03-05 22:39:53 +0100

    feat(): add markdown parser vite plugin chore(): update packages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+
+import Vue from "@vitejs/plugin-vue";
+import Markdown from "vite-plugin-md";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Markdown(),
+  ],
+});
