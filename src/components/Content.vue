0abf90c69b929e1cd5e344092f838fc2185fdeae
Ilyas Keskin <github.bzczr@simplelogin.co>
2025-10-25 22:37:11 +0200

    feat: add blog comp to main content

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,60 +2,61 @@
 import Logo from "./Logo.vue";
 import VerticalLine from "./VerticalLine.vue";
 import Menu from "./Menu.vue";
+import Blog from "./Blog.vue";
 </script>
 
 <template>
-  <div class="container">
-    <Logo />
-    <VerticalLine />
-    <Menu :items="items" />
-  </div>
-  <Resume />
+    <div class="container">
+        <Logo />
+        <VerticalLine />
+        <Menu :items="items" />
+    </div>
+    <Blog />
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      items: [
-        {
-          text: "LinkedIn",
-          icon: { family: "fab", name: "linkedin" },
-          link: "https://de.linkedin.com/in/ilyas-keskin-6697a518a",
-        },
-        {
-          text: "Xing",
-          icon: { family: "fab", name: "square-xing" },
-          link: "https://www.xing.com/profile/Ilyas_Keskin3",
-        },
-        {
-          text: "Credly",
-          icon: { family: "fas", name: "certificate" },
-          link: "https://www.credly.com/users/ilyas-keskin.a35bbfee/badges",
-        },
-      ],
-    };
-  },
+    data() {
+        return {
+            items: [
+                {
+                    text: "LinkedIn",
+                    icon: { family: "fab", name: "linkedin" },
+                    link: "https://de.linkedin.com/in/ilyas-keskin-6697a518a",
+                },
+                {
+                    text: "Xing",
+                    icon: { family: "fab", name: "square-xing" },
+                    link: "https://www.xing.com/profile/Ilyas_Keskin3",
+                },
+                {
+                    text: "Credly",
+                    icon: { family: "fas", name: "certificate" },
+                    link: "https://www.credly.com/users/ilyas-keskin.a35bbfee/badges",
+                },
+            ],
+        };
+    },
 };
 </script>
 
 <style scoped>
 .container {
-  display: flex;
-  flex-direction: row-reverse;
-  align-items: center;
-  justify-content: space-between;
-  min-width: 600px;
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    justify-content: space-between;
+    min-width: 600px;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
 }
 
 @media (max-width: 430px) {
-  .container {
-    flex-direction: column;
-    justify-content: center;
-    min-width: 0;
-  }
+    .container {
+        flex-direction: column;
+        justify-content: center;
+        min-width: 0;
+    }
 }
-</style>./Menu.vue
+</style>
