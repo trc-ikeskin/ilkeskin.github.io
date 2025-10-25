c65d13468b4cefbf8f7da95b5c47d6fbcb4a308f
Ilyas Keskin <github.bzczr@simplelogin.co>
2025-10-25 22:30:06 +0200

    feat: switch to vite-plugin-markdown

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from "vite";
+import * as path from "path";
 
 import Vue from "@vitejs/plugin-vue";
-import Markdown from "vite-plugin-md";
+import { plugin as mdPlugin, Mode } from "vite-plugin-markdown";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    Vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-    Markdown(),
-  ],
+    plugins: [
+        Vue({
+            include: [/\.vue$/],
+        }),
+        mdPlugin({
+            mode: [Mode.HTML, Mode.TOC, Mode.VUE],
+        }),
+    ],
 });
