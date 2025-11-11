ec9448d61091bd548b79388021273642c9b783a2
Ilyas Keskin <github.bzczr@simplelogin.co>
2025-11-11 20:43:57 +0100

    chore: remove unused markdown package

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 
 import Vue from "@vitejs/plugin-vue";
-import Markdown from "vite-plugin-md";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    Vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-    Markdown(),
-  ],
+    plugins: [
+        Vue({
+            include: [/\.vue$/, /\.md$/],
+        }),
+    ],
 });
